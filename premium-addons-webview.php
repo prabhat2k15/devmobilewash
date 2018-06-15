b2b3de128114d71231435ac42defcb8ff18296de
NazmurR <nazmur.r@gmail.com>
2018-06-16 01:05:46 +0600

    addon pages

---
+++ premium-addons-webview.php
@@ -0,0 +1,154 @@
+<html>
+<head>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link href='https://fonts.googleapis.com/css?family=Lato:400,700,300' rel='stylesheet' type='text/css'>
+<style>
+
+body{
+
+    font-family: 'Lato', sans-serif;
+margin: 0;
+padding: 0;
+    background: #000;
+    color: #fff;
+}
+
+h2.title{
+    text-align: center;
+    margin: 15px 0;
+}
+
+.blue-section{
+   background: #006bd0;
+    padding: 15px;
+    color: #fff;
+    text-align: center;
+    font-size: 20px;
+}
+
+.blue-section p{
+    margin: 0;
+}
+
+.addons-list, .addon-row{
+    width: 100%;
+    border-collapse: collapse;
+}
+
+.addons-list .row-wrapper{
+    background: #202020;
+    border-bottom: 2px solid #000;
+    padding: 10px;
+}
+
+.addon-row .alignright{
+    text-align: right;
+    padding-left: 5px;
+   
+}
+
+.addon-row h3{
+        margin: 0;
+    font-size: 22px;
+    margin-bottom: 10px;
+}
+
+.addon-row h3.price{
+    margin-bottom: 5px;
+}
+
+.addon-row p{
+    margin: 0;
+    color: #e4e4e4;
+}
+
+.addon-row .special{
+    font-size: 12px;
+    color: red;
+}
+</style>
+</head>
+<body>
+<h2 class="title">Premium Add-ons</h2>
+<div class="blue-section">
+    <p>Upgrade your package with the following add-ons</p>
+</div>
+<table class="addons-list">
+    <tr>
+        <td class="row-wrapper">
+            <table class="addon-row">
+             <tr>
+                <td>
+                    <h3>Clay Bar & Paste Wax</h3>
+                    <p>Remove embedded contaminants from surfaces & keep vehicle shinning for weeks</p>
+                </td>
+                <td class="alignright">
+                    <h3 class="price">$42.50</h3>
+                    <p class="special">RECOMMENDED</p>
+                </td>
+            </tr> 
+            </table>
+        </td>
+        
+    </tr>
+    <tr>
+        <td class="row-wrapper">
+            <table class="addon-row">
+             <tr>
+                <td>
+                    <h3>Water Spot Removal</h3>
+                    <p>Remove and protect exterior surfaces from stubborn water spots</p>
+                </td>
+                <td class="alignright">
+                    <h3 class="price">$30.00</h3>
+                    
+                </td>
+            </tr> 
+            </table>
+        </td>
+        
+    </tr>
+   
+    <tr>
+        <td class="row-wrapper">
+            <table class="addon-row">
+             <tr>
+                <td>
+                    <h3>Floor Mat Cleaning</h3>
+                    <p>Thorough cleaning of all floor mats</p>
+                </td>
+                <td class="alignright">
+                    <h3 class="price">$12.50</h3>
+                    
+                    
+                </td>
+            </tr> 
+            </table>
+        </td>
+        
+    </tr>
+
+    <tr>
+        <td class="row-wrapper">
+            <table class="addon-row">
+             <tr>
+                <td>
+                    <h3>Extra Cleaning Fee</h3>
+                    <p>Applied to orders with pet hair, debris, soil, etc. that may take longer to clean</p>
+                </td>
+                <td class="alignright">
+                    <h3 class="price">$10.00</h3>
+                    <p class="special">POPULAR</p>
+                    
+                </td>
+            </tr> 
+            </table>
+        </td>
+        
+    </tr>
+        
+</table>
+  
+</table>
+</body>
+</html>
