c6f9dd37291b1e1833cb6c4b7e261eac47450adb
NazmurR <nazmur.r@gmail.com>
2019-03-21 11:35:55 +0600

    cnr uncancel revert

--- admin-new/edit-order.php
+++ admin-new/edit-order.php
@@ -890,7 +890,7 @@ if ($getorder->transaction_id) {
                                     <?php endif; ?>
                                     <?php if (!$getorder->admin_submit_for_settle): ?>
                                         <?php if (($jsondata_permission->users_type == 'admin' || $jsondata_permission->users_type == 'scheduler')): ?>
-                                            <?php if (($getorder->status == 5) || ($getorder->status == 6) || ($getorder->status == 7)): ?>
+                                            <?php if (($getorder->status == 5) || ($getorder->status == 6)): ?>
                                                 <div style="float: right; font-size: 18px; margin-top: 3px; cursor: pointer; background: #e47e00; color: #fff; padding: 8px 35px; margin-right: 20px; margin-bottom: 15px;" class="wash-uncancel">Un-Cancel</div>
                                             <?php endif; ?>
                                             <?php if (($getorder->status == 4)): ?>
@@ -2478,7 +2478,11 @@ if ($getorder->transaction_id) {
 
                                                                                                                                                                                                                                                                                                                                                                         });
                                                                                                                                                                                                                                                                                                                                                                     },
-                                                                                                                                                                                                                                                                                                                                                                    "Customer No Response (fee)": function () {
+"Customer No Response (fee)": function () {
+  if(confirm('Are you sure you want to apply CNR fee to this order?')){
+
+
+                                                                                                                                                                                                                                                                                                                                                                      
                                                                                                                                                                                                                                                                                                                                                                         $(this).dialog("close");
                                                                                                                                                                                                                                                                                                                                                                         $(".cancel-order").html('Cancelling. Please wait...');
                                                                                                                                                                                                                                                                                                                                                                         $(".err-text").hide();
@@ -2493,6 +2497,7 @@ if ($getorder->transaction_id) {
                                                                                                                                                                                                                                                                                                                                                                                                         }
 
                                                                                                                                                                                                                                                                                                                                                                                                     });
+}
                                                                                                                                                                                                                                                                                                                                                                                                 }
 
                                                                                                                                                                                                                                                                                                                                                                                             }
