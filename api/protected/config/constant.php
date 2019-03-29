9f4080c1abade0412621fae07016c45e855442c3
SOlitaire Infosys <solitaireinfosys123@gmail.com>
2019-03-29 12:27:54 +0530

    29.03.2019 indentation of code config/*

--- api/protected/config/constant.php
+++ api/protected/config/constant.php
@@ -1,74 +1,73 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: rohan
  * Date: 11/26/2015
  * Time: 4:24 AM
  */
-
 /* --- API key --- */
 
-define("API_KEY","m4b5WB0h0HXUK8fTvPw5O1nIDDmEAt4c");
-define("API_KEY_CRON","zCWJmjHOlZDbsyiLwhwM0bDdB9sUo2yo");
-define("ROOT_URL","https://www.devmobilewash.com");
-define("ROOT_WEBFOLDER","/home/devmobilewash");
-define("COOKIE_DOMAIN",".devmobilewash.com");
-define("WEBSITE_URL","https://www.mobilewash.com");
-define("APP_ENV",""); // real or blank
-define("MW_SERVER_IP","209.95.41.9");
+define("API_KEY", "m4b5WB0h0HXUK8fTvPw5O1nIDDmEAt4c");
+define("API_KEY_CRON", "zCWJmjHOlZDbsyiLwhwM0bDdB9sUo2yo");
+define("ROOT_URL", "https://www.devmobilewash.com");
+define("ROOT_WEBFOLDER", "/home/devmobilewash");
+define("COOKIE_DOMAIN", ".devmobilewash.com");
+define("WEBSITE_URL", "https://www.mobilewash.com");
+define("APP_ENV", ""); // real or blank
+define("MW_SERVER_IP", "209.95.41.9");
 
-define("ENV","sandbox");//sandbox or production
-define("MERCHANT_ID","czckz7jkzcnny4jj");
-define("MERCHANT_ID_REAL","74zsnfqy5svgpvjv");
-define("PUBLIC_KEY","zwcjr8h49b5j5s96");
-define("PRIVATE_KEY","1d9f980b86df0a4d0e0ce3253970a8ee");
-define("MERCHANT_ACCOUNT_ID","");
-define("BT_TRANSACTION_URL","https://sandbox.braintreegateway.com/merchants/czckz7jkzcnny4jj/transactions/");
+define("ENV", "sandbox"); //sandbox or production
+define("MERCHANT_ID", "czckz7jkzcnny4jj");
+define("MERCHANT_ID_REAL", "74zsnfqy5svgpvjv");
+define("PUBLIC_KEY", "zwcjr8h49b5j5s96");
+define("PRIVATE_KEY", "1d9f980b86df0a4d0e0ce3253970a8ee");
+define("MERCHANT_ACCOUNT_ID", "");
+define("BT_TRANSACTION_URL", "https://sandbox.braintreegateway.com/merchants/czckz7jkzcnny4jj/transactions/");
 
 
 //AES-256-CBC keys
 
-define("AES256CBC_PASSPHRASE","buckshotherniadiwangatflamcephalad");
-define("AES256CBC_KEY","C100C17EE98435861499D347578F93970B462B8ECA86744B");
-define("AES256CBC_IV","6877393754561BEA7C8FC5693A6D05BA");
-define("AES256CBC_SALT","928FC69A87CFFF2F");
-define("AES128CBC_KEY","RfUjWnZr4u7x!A%D");
-define("AES128CBC_IV","u7x!A%D*G-KaPdSg");
+define("AES256CBC_PASSPHRASE", "buckshotherniadiwangatflamcephalad");
+define("AES256CBC_KEY", "C100C17EE98435861499D347578F93970B462B8ECA86744B");
+define("AES256CBC_IV", "6877393754561BEA7C8FC5693A6D05BA");
+define("AES256CBC_SALT", "928FC69A87CFFF2F");
+define("AES128CBC_KEY", "RfUjWnZr4u7x!A%D");
+define("AES128CBC_IV", "u7x!A%D*G-KaPdSg");
 define("AES256CBC_STATUS", 1); // 0 => encrypt/decrypt off, 1 => encrypt/decrypt on
-define("AES256CBC_API_PASS","33CkqOl7C*4iO5Q.W8aO2eCYQ749=%158Jt8mIbM");
-define("AES256CBC_API_PASS_V2_2","AE22K5O6j15xY3Qg5Oh989n6v63bS8eJ");
+define("AES256CBC_API_PASS", "33CkqOl7C*4iO5Q.W8aO2eCYQ749=%158Jt8mIbM");
+define("AES256CBC_API_PASS_V2_2", "AE22K5O6j15xY3Qg5Oh989n6v63bS8eJ");
 
 /*
-define("ENV","production");//sandbox or production
-define("MERCHANT_ID","74zsnfqy5svgpvjv");
-define("PUBLIC_KEY","7gg5kfvkx8w5fcx8");
-define("PRIVATE_KEY","579e6af0c752079c2f9596c838191327");
-define("MERCHANT_ACCOUNT_ID","");
-*/
+  define("ENV","production");//sandbox or production
+  define("MERCHANT_ID","74zsnfqy5svgpvjv");
+  define("PUBLIC_KEY","7gg5kfvkx8w5fcx8");
+  define("PRIVATE_KEY","579e6af0c752079c2f9596c838191327");
+  define("MERCHANT_ACCOUNT_ID","");
+ */
 //TWILIO
-
 //
 
-define("TWILIO_SID",'ACa9a7569fc80a0bd3a709fb6979b19423'); 
-define("TWILIO_AUTH_TOKEN","c2644f802b56217189ae58736e246e69"); 
-define("VERSION",'2010-04-01');
+define("TWILIO_SID", 'ACa9a7569fc80a0bd3a709fb6979b19423');
+define("TWILIO_AUTH_TOKEN", "c2644f802b56217189ae58736e246e69");
+define("VERSION", '2010-04-01');
 
-define("AWS_ACCESS_KEY",'AKIAJ6HPIJ2U66GFEPSA'); 
-define("AWS_SECRET_KEY","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");
-define("AWS_CUST_IOS_PLATFORM_ARN","arn:aws:sns:us-west-2:461900685840:app/GCM/custiosdevices_dev");
-define("AWS_CUST_ANDROID_PLATFORM_ARN","arn:aws:sns:us-west-2:461900685840:app/GCM/custandroiddevices_dev");
-define("AWS_WASHER_IOS_PLATFORM_ARN","arn:aws:sns:us-west-2:461900685840:app/GCM/washeriosdevices_dev");
-define("AWS_WASHER_ANDROID_PLATFORM_ARN","arn:aws:sns:us-west-2:461900685840:app/GCM/washerandroiddevices_dev");
+define("AWS_ACCESS_KEY", 'AKIAJ6HPIJ2U66GFEPSA');
+define("AWS_SECRET_KEY", "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");
+define("AWS_CUST_IOS_PLATFORM_ARN", "arn:aws:sns:us-west-2:461900685840:app/GCM/custiosdevices_dev");
+define("AWS_CUST_ANDROID_PLATFORM_ARN", "arn:aws:sns:us-west-2:461900685840:app/GCM/custandroiddevices_dev");
+define("AWS_WASHER_IOS_PLATFORM_ARN", "arn:aws:sns:us-west-2:461900685840:app/GCM/washeriosdevices_dev");
+define("AWS_WASHER_ANDROID_PLATFORM_ARN", "arn:aws:sns:us-west-2:461900685840:app/GCM/washerandroiddevices_dev");
 
 // wash request staus
 
-define("WASHREQUEST_STATUS_PENDING",'0');
-define("WASHREQUEST_STATUS_ACCEPTED",'1');
-define("WASHREQUEST_STATUS_AGENTARRIVED",'2');
-define("WASHREQUEST_STATUS_AGENTARRIVED_CONFIRMED_BYCLIENT",'3');
-define("WASHREQUEST_STATUS_COMPLETEWASH",'4');
-define("WASHREQUEST_STATUS_CANCELWASH_BYCLIENT",'5');
-define("WASHREQUEST_STATUS_CANCELWASH_BYAGENT",'6');
+define("WASHREQUEST_STATUS_PENDING", '0');
+define("WASHREQUEST_STATUS_ACCEPTED", '1');
+define("WASHREQUEST_STATUS_AGENTARRIVED", '2');
+define("WASHREQUEST_STATUS_AGENTARRIVED_CONFIRMED_BYCLIENT", '3');
+define("WASHREQUEST_STATUS_COMPLETEWASH", '4');
+define("WASHREQUEST_STATUS_CANCELWASH_BYCLIENT", '5');
+define("WASHREQUEST_STATUS_CANCELWASH_BYAGENT", '6');
 // given by sanket.
 #define WASHREQUEST_STATUS_PENDING 0
 #define WASHREQUEST_STATUS_ACCEPTED 1
@@ -81,5 +80,6 @@ define("WASHREQUEST_STATUS_CANCELWASH_BYAGENT",'6');
 defined('DATE_TIME') OR define('DATE_TIME', date('Y-m-d H:i:s'));
 
 // washer push api key
-
-define('API_ACCESS_KEY_ANDRIOD', 'AAAAKHWvBtc:APA91bH7eWGNgvoZQxe56zzxeE2cxW4qVG_5dc9iwpF73R0ph0govruyXQ-1QK-pE_VxLeBewkXsnKWecuVp42IZKJSB0Z6yo5x44w6ytelM7HXWHSItSViPO4TmzscYddTEmcqNi3ae');
+//define('API_ACCESS_KEY_ANDRIOD', 'AAAAr9Rb1jw:APA91bEe8fM-QrksVvc8PyGg2o8nAKOQlSXLHfVDCxurWQS8PtZsBLBAp10IXPhykycAD7Wci_R15kjCVlUtdgECXCsvJw8IOAwvbrlaWeQ_HEhaNcT005ZPWmYwBoPZHDk-msA1ELAV');
+//define('API_ACCESS_KEY_ANDRIOD', 'AAAAKHWvBtc:APA91bH7eWGNgvoZQxe56zzxeE2cxW4qVG_5dc9iwpF73R0ph0govruyXQ-1QK-pE_VxLeBewkXsnKWecuVp42IZKJSB0Z6yo5x44w6ytelM7HXWHSItSViPO4TmzscYddTEmcqNi3ae');
+define('API_ACCESS_KEY_ANDRIOD', 'AAAA0cngH9I:APA91bFIS9qSNsOWvf3Mefnd9l9Z4QaURxerPJnCf9EKUGdmm9AGYVOI-G7zcHWt9dIuX2UrIXCNVfkzXHWAI3al7EVwBoMi__n2GXduvSMxqPJSi30acnTwzhNa7mtzO6rOn-pJXsep');
--- api/protected/config/main.php
+++ api/protected/config/main.php
@@ -1,80 +1,76 @@
 <?php
+
 include(dirname(__FILE__) . '/constant.php');
 
 // uncomment the following to define a path alias
 // Yii::setPathOfAlias('local','path/to/local-folder');
-
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'MobileWash',
-	'theme'=> 'varg',
-	'preload'=>array('log'),
- 'timeZone' => 'America/Los_Angeles',
-
-	// auto loading model and component classes
-	'import'=>array(
-		'application.models.*',
-		'application.components.*',
-		'ext.YiiMailer.YiiMailer',
-          'ext.braintree.CustomBraintree',
-
-	),
-
-	'modules'=>array(
-		'admin',
-	),
-
-	// application components
-	'components'=>array(
-		'user'=>array(
-			// enable cookie-based authentication
-			'allowAutoLogin'=>true,
-		),
-		//	Database configuration
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=devmobil_mwmain',
-			'emulatePrepare' => true,
-			'username' => 'devmobil_mwuser',
-			'password' => '9F;WPnZwCEscQ$*[K4',
-			'charset' => 'utf8mb4',
-		),
-		'braintree' => array(
-            'class'=>'application.extensions.braintree.CustomBraintree',
-            'ENV' => ENV,//sandbox or production
+    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+    'name' => 'MobileWash',
+    'theme' => 'varg',
+    'preload' => array('log'),
+    'timeZone' => 'America/Los_Angeles',
+    // auto loading model and component classes
+    'import' => array(
+        'application.models.*',
+        'application.components.*',
+        'ext.YiiMailer.YiiMailer',
+        'ext.braintree.CustomBraintree',
+    ),
+    'modules' => array(
+        'admin',
+    ),
+    // application components
+    'components' => array(
+        'user' => array(
+            // enable cookie-based authentication
+            'allowAutoLogin' => true,
+        ),
+        //	Database configuration
+        'db' => array(
+            'connectionString' => 'mysql:host=localhost;dbname=devmobil_mwmain',
+            'emulatePrepare' => true,
+            'username' => 'devmobil_mwuser',
+            'password' => '9F;WPnZwCEscQ$*[K4',
+            'charset' => 'utf8mb4',
+        ),
+        'braintree' => array(
+            'class' => 'application.extensions.braintree.CustomBraintree',
+            'ENV' => ENV, //sandbox or production
             'MERCHANT_ID' => MERCHANT_ID,
-            'MERCHANT_ACCOUNT_ID'=>MERCHANT_ACCOUNT_ID,
+            'MERCHANT_ACCOUNT_ID' => MERCHANT_ACCOUNT_ID,
             'PUBLIC_KEY' => PUBLIC_KEY,
-            'PRIVATE_KEY'=>PRIVATE_KEY
+            'PRIVATE_KEY' => PRIVATE_KEY
         ),
-'twilio' => array(
-            'class'=>'application.extensions.twilio.CustomTwilio',
+        'twilio' => array(
+            'class' => 'application.extensions.twilio.CustomTwilio',
             'SID' => SID,
-            'CLIENT_TOKEN'=>CLIENT_TOKEN,
-            'VERSION'=>VERSION
+            'CLIENT_TOKEN' => CLIENT_TOKEN,
+            'VERSION' => VERSION
         ),
-		'errorHandler'=>array(
-			// use 'site/error' action to display errors
-			'errorAction'=>'site/error',
-		),
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, waiting',
-				),
-			),
-		),
-	),
-	'params'=>array(
-		//	Global Admin Email
-		/*'adminEmail'=>'mobilewash8@gmail.com',
-		'adminToEmail'=>'mobilewash8@gmail.com',*/
-                'AdminToEmailFeedBack'=>'feedback@mobilewash.com',
-                'adminEmail'=>'admin@mobilewash.com',
-		'adminToEmail'=>'admin@mobilewash.com',
-		'adminFromEmail'=>'info@mobilewash.com'
-	),
+        'errorHandler' => array(
+            // use 'site/error' action to display errors
+            'errorAction' => 'site/error',
+        ),
+        'log' => array(
+            'class' => 'CLogRouter',
+            'routes' => array(
+                array(
+                    'class' => 'CFileLogRoute',
+                    'levels' => 'error, waiting',
+                ),
+            ),
+        ),
+    ),
+    'params' => array(
+        //	Global Admin Email
+        /* 'adminEmail'=>'mobilewash8@gmail.com',
+          'adminToEmail'=>'mobilewash8@gmail.com', */
+        'AdminToEmailFeedBack' => 'feedback@mobilewash.com',
+        'adminEmail' => 'admin@mobilewash.com',
+        'adminToEmail' => 'admin@mobilewash.com',
+        'adminFromEmail' => 'info@mobilewash.com'
+    ),
 );
